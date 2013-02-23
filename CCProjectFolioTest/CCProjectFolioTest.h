13195f8568f92730f30adbb8a66558da32ed754c
Kenneth Cluff <kennethcluff@theMini.local>
2013-02-23 12:13:54 -0700

    Implementing test driven development

---
+++ CCProjectFolioTest/CCProjectFolioTest.h
@@ -0,0 +1,13 @@
+//
+//  CCProjectFolioTest.h
+//  CCProjectFolioTest
+//
+//  Created by Kenneth Cluff on 2/20/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CCProjectFolioTest : SenTestCase
+
+@end
---
+++ CCProjectFolioTest/CCProjectFolioTest.m
@@ -0,0 +1,32 @@
+//
+//  CCProjectFolioTest.m
+//  CCProjectFolioTest
+//
+//  Created by Kenneth Cluff on 2/20/13.
+//
+//
+
+#import "CCProjectFolioTest.h"
+
+@implementation CCProjectFolioTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in CCProjectFolioTest");
+}
+
+@end
